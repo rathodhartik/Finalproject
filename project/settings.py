e4d1cbbbf460420521d093ad4f040fd223f30dc1
Hartik <rathodhartik@gmail.com>
2022-02-22 13:38:28 +0530

    final donee

--- project/settings.py
+++ project/settings.py
@@ -113,12 +113,25 @@ AUTH_USER_MODEL = 'finalapp.User'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'finalproject',
+        'USER': 'admin',
+        'PASSWORD': 'Demo@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
 
 
 # Password validation
